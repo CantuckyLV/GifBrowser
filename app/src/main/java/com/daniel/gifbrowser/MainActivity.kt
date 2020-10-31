7af14b5aaffa199c2b0237ecb9b4892e50269ac2
CantuckyLV <danielgc0794@gmail.com>
2020-10-31 16:06:41 -0700

    added first fragment

--- app/src/main/java/com/daniel/gifbrowser/MainActivity.kt
+++ app/src/main/java/com/daniel/gifbrowser/MainActivity.kt
@@ -2,11 +2,28 @@ package com.daniel.gifbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    //private  lateinit var viewModel : MainActivityViewModel
+    private lateinit var flFragmentPH : FrameLayout
+    //private var currentlyDisplayed = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        flFragmentPH = findViewById(R.id.fl_fragment_ph)
+        //viewModel = MainActivityViewModel()
+        //viewModel.requestCOmmits()!!.observe(this, Observer<ArrayList<CommitDetail>> { posts -> showCommits(posts) })
+    }
+
+    fun showTrendyGifsTemp(){
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        val frag = CommitsFragment()
+        //frag.arguments = bundle
+        fragmentTransaction.replace(R.id.fl_fragment_ph, frag,"fragment_ph_fl")
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/daniel/gifbrowser/ViewModel/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.daniel.gifbrowser.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.ArrayList
+
+/**
+ * ViewModel Class for main activity
+ */
+class MainActivityViewModel : ViewModel(){
+}
